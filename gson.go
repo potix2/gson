e4ba3d80c14c4cbdc95e7b60e3b2c1d2a888112c
Katsunori Kanda <potix2@gmail.com>
2020-08-22 13:10:24 +0900

    Extract functions for each tokens

--- gson.go
+++ gson.go
@@ -2,31 +2,80 @@ package gson
 
 import "fmt"
 
-func parseBytes(bytes []byte) (interface{}, error) {
-	var result interface{}
-	pos := 0
-	for pos < len(bytes) {
-		if bytes[pos] == '"' {
-			startString := pos + 1
-			endOfString := len(bytes) - 1
-			result = string(bytes[startString:endOfString])
-			pos = len(bytes)
-		} else if len(bytes)-pos >= 4 && bytes[pos] == 'n' && bytes[pos+1] == 'u' && bytes[pos+2] == 'l' && bytes[pos+3] == 'l' {
-			result = nil
-			pos += 4
-		} else if len(bytes)-pos >= 4 && bytes[pos] == 't' && bytes[pos+1] == 'r' && bytes[pos+2] == 'u' && bytes[pos+3] == 'e' {
-			result = true
-			pos += 4
-		} else if len(bytes)-pos >= 5 && bytes[pos] == 'f' && bytes[pos+1] == 'a' && bytes[pos+2] == 'l' && bytes[pos+3] == 's' && bytes[pos+4] == 'e' {
-			result = false
-			pos += 5
-		} else {
-			return nil, fmt.Errorf("unknown token")
-		}
+func isWhitespace(b byte) bool {
+	return b == ' ' || b == '\n' || b == '\r' || b == '\t'
+}
+
+func skipWhitespace(bytes []byte, pos int) int {
+	for isWhitespace(bytes[pos]) {
+		pos++
 	}
-	return result, nil
+	return pos
+}
+
+func parseValue(bytes []byte, pos int) (interface{}, int, error) {
+	pos = skipWhitespace(bytes, pos)
+	return nextToken(bytes, pos)
+}
+
+func nextToken(bytes []byte, pos int) (interface{}, int, error) {
+	if bytes[pos] == '"' {
+		return parseString(bytes, pos)
+	} else if bytes[pos] == 'n' {
+		return parseNull(bytes, pos)
+	} else if bytes[pos] == 't' {
+		return parseTrue(bytes, pos)
+	} else if bytes[pos] == 'f' {
+		return parseFalse(bytes, pos)
+	} else if bytes[pos] == '[' {
+		return parseArray(bytes, pos)
+	} else {
+		return nil, -1, fmt.Errorf("unknown token")
+	}
+}
+
+func parseNull(bytes []byte, pos int) (interface{}, int, error) {
+	if len(bytes)-pos >= 4 && bytes[pos] == 'n' && bytes[pos+1] == 'u' && bytes[pos+2] == 'l' && bytes[pos+3] == 'l' {
+		return nil, pos + 4, nil
+	} else {
+		return nil, pos, fmt.Errorf("unknown token")
+	}
+}
+
+func parseTrue(bytes []byte, pos int) (interface{}, int, error) {
+	if len(bytes)-pos >= 4 && bytes[pos] == 't' && bytes[pos+1] == 'r' && bytes[pos+2] == 'u' && bytes[pos+3] == 'e' {
+		return true, pos + 4, nil
+	} else {
+		return nil, pos, fmt.Errorf("unknown token")
+	}
+}
+
+func parseFalse(bytes []byte, pos int) (interface{}, int, error) {
+	if len(bytes)-pos >= 5 && bytes[pos] == 'f' && bytes[pos+1] == 'a' && bytes[pos+2] == 'l' && bytes[pos+3] == 's' && bytes[pos+4] == 'e' {
+		return false, pos + 5, nil
+	} else {
+		return nil, pos, fmt.Errorf("unknown token")
+	}
+}
+
+func parseString(bytes []byte, pos int) (interface{}, int, error) {
+	start := pos + 1
+	end := start
+	for end = start; bytes[end] != '"'; end++ {
+	}
+	result := string(bytes[start:end])
+	return result, end + 1, nil
+}
+
+func parseArray(bytes []byte, pos int) (interface{}, int, error) {
+	if len(bytes)-pos >= 2 && bytes[pos] == '[' && bytes[pos+1] == ']' {
+		return []interface{}{}, pos + 2, nil
+	} else {
+		return nil, pos, fmt.Errorf("unknown token")
+	}
 }
 
 func Parse(text string) (interface{}, error) {
-	return parseBytes([]byte(text))
+	result, _, err := parseValue([]byte(text), 0)
+	return result, err
 }
--- gson_test.go
+++ gson_test.go
@@ -16,19 +16,14 @@ func TestParse(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "single character",
-			args: args{text: `"a"`},
+			name: "string with whitespace",
+			args: args{text: ` "a"`},
 			want: "a",
 		},
 		{
-			name: "empty string",
-			args: args{text: `""`},
-			want: "",
-		},
-		{
-			name: "multiple character",
-			args: args{text: `"ab1234"`},
-			want: "ab1234",
+			name: "trim string",
+			args: args{text: `"a" `},
+			want: "a",
 		},
 		{
 			name: "null",
@@ -50,6 +45,11 @@ func TestParse(t *testing.T) {
 			args: args{text: `false`},
 			want: false,
 		},
+		{
+			name: "empty array",
+			args: args{text: `[]`},
+			want: []interface{}{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -60,6 +60,59 @@ func TestParse(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Parse() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseString(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		want1   int
+		wantErr bool
+	}{
+		{
+			name:  "single character",
+			args:  args{text: `"a"`},
+			want:  "a",
+			want1: 3,
+		},
+		{
+			name:  "trim string",
+			args:  args{text: `"a" `},
+			want:  "a",
+			want1: 3,
+		},
+		{
+			name:  "empty string",
+			args:  args{text: `""`},
+			want:  "",
+			want1: 2,
+		},
+		{
+			name:  "multiple character",
+			args:  args{text: `"ab1234"`},
+			want:  "ab1234",
+			want1: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := parseString([]byte(tt.args.text), 0)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseString() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("parseString() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
